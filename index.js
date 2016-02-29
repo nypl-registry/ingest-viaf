593f70b55a2b42db6ac85c1b575959807c7cca0d
Matt Miller <thisismattmiller@gmail.com>
2016-02-29 16:23:28 -0500

    string viaf ids and clean exit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 function IngestViaf() {
 	this.ingestCluster = require(__dirname + '/lib/ingest_clusters')(this)
+	this.ingestPersist = require(__dirname + '/lib/ingest_persist')(this)
 }
 
 module.exports = exports = new IngestViaf();
