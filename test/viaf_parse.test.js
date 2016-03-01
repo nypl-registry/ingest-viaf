7e004be2fdfccc72e5e1e6b83f0977de9207b158
Matt Miller <thisismattmiller@gmail.com>
2016-03-01 00:12:08 -0500

    viaf persist

--- test/viaf_parse.test.js
+++ test/viaf_parse.test.js
@@ -3,14 +3,14 @@ var viafParse = require('../lib/viaf_parse.js')
 var fs = require("fs");
 
 
-
+var persistXml = '<?xml version="1.0"?><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:p="http://viaf.org/viaf/abandonedViafRecord">  <rdf:Description rdf:about="http://viaf.org/viaf/305998434">    <owl:sameAs rdf:resource="http://viaf.org/viaf/76719094"/>  </rdf:Description></rdf:RDF>'
+var badPersistXml = '<?xml version="1.0"?><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:p="http://viaf.org/viaf/abandonedViafRecord"/>'
 
 describe('viaf parse', function () {
 
 	var xml = fs.readFileSync('test/viaf_cluster_2016_02.xml', 'utf8');
 
 	var xmlDoc= viafParse.parseXml(xml)
-
 
 	it('extractSourceCount', function () {
 		viafParse.extractSourceCount(xmlDoc).should.equal(28)
@@ -52,6 +52,11 @@ describe('viaf parse', function () {
 		viafParse.extractDbpediaId(xmlDoc).should.equal("Jack_Kerouac")
 	})
 
+	it('returnPersist', function () {
+		viafParse.returnPersist(persistXml).oldId.should.equal("305998434")
+		viafParse.returnPersist(badPersistXml).should.equal("")
+
+	})
 
 
 })
