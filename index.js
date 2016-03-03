e8fdb45616968de696118b4fdb1e9aab2e93b281
Matt Miller <thisismattmiller@gmail.com>
2016-03-03 14:14:54 -0500

    standard lint added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-"use strict"
+'use strict'
 
-function IngestViaf() {
-	this.ingestCluster = require(__dirname + '/lib/ingest_clusters')(this)
-	this.ingestPersist = require(__dirname + '/lib/ingest_persist')(this)
+function IngestViaf () {
+  /**
+   * Will prompt for the location of the VIAF cluster split file and fork a process for each file bulk inserting it into the registry-ingest database
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.ingestCluster = require(`${__dirname}/lib/ingest_clusters`)(this)
+
+  /**
+   * Will do the same prompt and update the existing cluster with previous ids that redirect to it.
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.ingestPersist = require(`${__dirname}/lib/ingest_persist`)(this)
 }
 
-module.exports = exports = new IngestViaf();
+module.exports = exports = new IngestViaf()
--- test/viaf_parse.test.js
+++ test/viaf_parse.test.js
@@ -1,62 +1,58 @@
 var should = require('should')
 var viafParse = require('../lib/viaf_parse.js')
-var fs = require("fs");
+var fs = require('fs')
 
 
 var persistXml = '<?xml version="1.0"?><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:p="http://viaf.org/viaf/abandonedViafRecord">  <rdf:Description rdf:about="http://viaf.org/viaf/305998434">    <owl:sameAs rdf:resource="http://viaf.org/viaf/76719094"/>  </rdf:Description></rdf:RDF>'
 var badPersistXml = '<?xml version="1.0"?><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:p="http://viaf.org/viaf/abandonedViafRecord"/>'
 
 describe('viaf parse', function () {
+  var xml = fs.readFileSync('test/viaf_cluster_2016_02.xml', 'utf8')
 
-	var xml = fs.readFileSync('test/viaf_cluster_2016_02.xml', 'utf8');
+  var xmlDoc = viafParse.parseXml(xml)
 
-	var xmlDoc= viafParse.parseXml(xml)
+  it('extractSourceCount', function () {
+    viafParse.extractSourceCount(xmlDoc).should.equal(28)
+  })
+  it('hasLc', function () {
+    viafParse.hasLc(xmlDoc).should.equal(true)
+  })
+  it('hasDbn', function () {
+    viafParse.hasDbn(xmlDoc).should.equal(true)
+  })
+  it('extractLcId', function () {
+    viafParse.extractLcId(xmlDoc).should.equal('n80036674')
+  })
+  it('extractGettyId', function () {
+    viafParse.extractGettyId(xmlDoc).should.equal('500290917')
+  })
+  it('extractType', function () {
+    viafParse.extractType(xmlDoc).should.equal('Personal')
+  })
+  it('extractLcTerm', function () {
+    viafParse.extractLcTerm(xmlDoc).should.equal('Kerouac, Jack, 1922-1969')
+  })
+  it('extractDnbTerm', function () {
+    viafParse.extractDnbTerm(xmlDoc).should.equal('Kerouac, Jack, 1922-1969')
+  })
+  it('extractViafTerm', function () {
+    viafParse.extractViafTerm(xmlDoc).should.equal('Kerouac, Jack, 1922-1969')
+  })
+  it('extractWikidataId', function () {
+    viafParse.extractWikidataId(xmlDoc).should.equal('Q160534')
+  })
+  it('extractBirth', function () {
+    viafParse.extractBirth(xmlDoc).should.equal('1922-03-12')
+  })
+  it('extractDeath', function () {
+    viafParse.extractDeath(xmlDoc).should.equal('1969-10-21')
+  })
+  it('extractDbpediaId', function () {
+    viafParse.extractDbpediaId(xmlDoc).should.equal('Jack_Kerouac')
+  })
 
-	it('extractSourceCount', function () {
-		viafParse.extractSourceCount(xmlDoc).should.equal(28)
-	})
-	it('hasLc', function () {
-		viafParse.hasLc(xmlDoc).should.equal(true)
-	})
-	it('hasDbn', function () {
-		viafParse.hasDbn(xmlDoc).should.equal(true)
-	})
-	it('extractLcId', function () {
-		viafParse.extractLcId(xmlDoc).should.equal("n80036674")
-	})
-	it('extractGettyId', function () {
-		viafParse.extractGettyId(xmlDoc).should.equal("500290917")
-	})
-	it('extractType', function () {
-		viafParse.extractType(xmlDoc).should.equal("Personal")
-	})
-	it('extractLcTerm', function () {
-		viafParse.extractLcTerm(xmlDoc).should.equal("Kerouac, Jack, 1922-1969")
-	})
-	it('extractDnbTerm', function () {
-		viafParse.extractDnbTerm(xmlDoc).should.equal("Kerouac, Jack, 1922-1969")
-	})
-	it('extractViafTerm', function () {
-		viafParse.extractViafTerm(xmlDoc).should.equal("Kerouac, Jack, 1922-1969")
-	})
-	it('extractWikidataId', function () {
-		viafParse.extractWikidataId(xmlDoc).should.equal("Q160534")
-	})
-	it('extractBirth', function () {
-		viafParse.extractBirth(xmlDoc).should.equal("1922-03-12")
-	})
-	it('extractDeath', function () {
-		viafParse.extractDeath(xmlDoc).should.equal("1969-10-21")
-	})
-	it('extractDbpediaId', function () {
-		viafParse.extractDbpediaId(xmlDoc).should.equal("Jack_Kerouac")
-	})
-
-	it('returnPersist', function () {
-		viafParse.returnPersist(persistXml).oldId.should.equal("305998434")
-		viafParse.returnPersist(badPersistXml).should.equal("")
-
-	})
-
-
+  it('returnPersist', function () {
+    viafParse.returnPersist(persistXml).oldId.should.equal('305998434')
+    viafParse.returnPersist(badPersistXml).should.equal('')
+  })
 })
